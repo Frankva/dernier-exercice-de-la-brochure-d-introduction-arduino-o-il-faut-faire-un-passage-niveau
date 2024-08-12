72f53ca96fa5a1e59fa65f0a8e7d8731046b32a2
Frankva <24254885+Frankva@users.noreply.github.com>
2024-08-12 16:53:20 +0200

    refactor: change c struct to c++ class

--- 13.ino
+++ 13.ino
@@ -1,8 +1,5 @@
 #include <Servo.h>
 
-void invokeFailureMode();
-void invokeAutomaticMode();
-void invokeManualMode();
 void print(String toPrint="");
 void print(long numberToPrint);
 String input(String toPrint="");
@@ -13,60 +10,164 @@ const unsigned int RED_LED = 12;
 const unsigned int GREEN_LED = 13;
 unsigned long lastExecutedMillis = 0;
 
-typedef struct LevelCrossingGate {
-  unsigned char position;
-  unsigned char minimum;
-  unsigned char maximum;
-  unsigned char middle;
-  Servo servo;
-} LevelCrossingGate;
+class LevelCrossingGate
+{
+  private:
+    unsigned char position;
+    unsigned char minimum;
+    unsigned char maximum;
+    unsigned char middle;
+    Servo servo;
 
-enum {
-  failure, automatic, manual
+  public:
+    void LevelCrossingGate() {
+      this->minimum = 2;
+      this->maximum = 178;
+      this->middle = 90;
+      this->servo.attach(5);
+    }
+    void levelCrossingGateOpen() {
+      if (this->position == this->minimum) {
+        return;
+      }
+      this->position = this->minimum;
+      this->servo.write(this->position);
+    }
+    void levelCrossingGateClose() {
+      if (this->position == this->minimum) {
+        return;
+      }
+      this->position = this->middle;
+      this->servo.write(this->position);
+    }
 };
 
-typedef struct Mode {
-  void (*invoke)(void);
-} Mode;
 
+class Mode
+{
+  public:
+    virtual void invoke() = 0;
+};
 
-LevelCrossingGate gate;
-Mode failureMode;
-Mode automaticMode;
-Mode manualMode;
-Mode modes[3];
-unsigned char currentMode;
+class FailureMode : public Mode
+{
+  public:
+    void invoke() {
+      print("invokeFailureMode");
+      blinkTwoLed();
+      levelCrossingGateOpen(&gate);
+      if (Serial.available() == 0) {
+        return;
+      }
+      String answer = Serial.readStringUntil('\n');
+      if (answer == "a") {
+        currentMode = automatic;
+        return;
+      }
+      print("Signal invalide");
+    }
+
+};
+
+class AutomaticMode: public Mode
+{
+  private:
+    Mode modes[2];
+  public:
+    AutomaticMode() {
+
+    }
+    void invoke() {
+      print("invokeAutomaticMode");
+      invokeAutomaticRedMode();
+      String answer = input("vert ou manuel ?");
+      if (answer == "vert") {
+        // TODO refactory that because call stack
+        invokeAutomaticGreenMode();
+        return;
+      }
+      if (answer == "manuel") {
+        currentMode = manual;
+        return;
+      }
+      print("Données erronées");
+      currentMode = failure;
+    }
+    void invokeAutomaticRedMode() {
+      print("Mode rouge");
+      digitalWrite(GREEN_LED, false);
+      digitalWrite(RED_LED, true);
+      levelCrossingGateClose(&gate);
+    }
+
+    void invokeAutomaticGreenMode() {
+      print("Mode vert");
+      digitalWrite(RED_LED, false);
+      digitalWrite(GREEN_LED, true);
+      levelCrossingGateOpen(&gate);
+      // TODO refactory that because call stack
+      String answer = input("rouge ou manuel ?");
+      if (answer == "rouge") {
+        invokeAutomaticRedMode();
+        return;
+      }
+      if (answer == "manuel") {
+        currentMode = manual;
+        return;
+      }
+      print("Données erronées");
+      currentMode = failure;
+    }
+};
+
+class ManualMode : public Mode
+{
+};
+
+typedef enum {
+  failure, automatic, manual
+} ModeName;
+
+class App
+{
+  private:
+    Mode modes[ModeName::manual + 1];
+    unsigned char currentMode;
+    LevelCrossingGate gate();
+  public:
+    App() {
+      print("App");
+      FailureMode failureMode();
+      AutomaticMode automaticMode();
+      ManualMode = manualMode();
+      modes[failure] = failureMode;
+      modes[automatic] = automaticMode;
+      modes[manual] = manualMode;
+      currentMode = failure;
+      pinMode(BOUTON, INPUT);
+      pinMode(RED_LED, OUTPUT);
+      pinMode(GREEN_LED, OUTPUT);
+      Serial.begin(9600);
+      print();
+    }
+    void update() {
+      print("App invoke");
+      modes[currentMode].invoke();
+    }
+    
+};
+
+App app();
 
 void setup() {
   print("setup");
-  LevelCrossingGateInit(&gate);
-  failureMode.invoke = invokeFailureMode;
-  automaticMode.invoke = invokeAutomaticMode;
-  manualMode.invoke = invokeManualMode;
-  modes[0] = failureMode;
-  modes[1] = automaticMode;
-  modes[2] = manualMode;
-  currentMode = failure;
-
-  pinMode(BOUTON, INPUT);
-  pinMode(RED_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);
-  Serial.begin(9600);
-  print();
 } 
 
 void loop() {
   print("loop");
-  modes[currentMode].invoke();
-  
+  app.update();
 } 
 
-void LevelCrossingGateInit(LevelCrossingGate *gate) {
-  gate->minimum = 2;
-  gate->maximum = 178;
-  gate->middle = 90;
-  gate->servo.attach(5);
-}
 
 void allumeLed(bool etatBouton) {
   if (etatBouton) {
@@ -87,69 +188,9 @@ void print(long numberToPrint) {
   Serial.println(String(numberToPrint));
 }
 
-void invokeFailureMode() {
-  print("invokeFailureMode");
-  blinkTwoLed();
-  levelCrossingGateOpen(&gate);
 
-  if (Serial.available() == 0) {
-    return;
-  }
-  String answer = Serial.readStringUntil('\n');
-  if (answer == "a") {
-    currentMode = automatic;
-    return;
-  }
-  print("Signal invalide");
-}
 
-void invokeAutomaticMode() {
-  print("invokeAutomaticMode");
-  invokeAutomaticRedMode();
-  String answer = input("vert ou manuel ?");
-  if (answer == "vert") {
-    // TODO refactory that because call stack
-    invokeAutomaticGreenMode();
-    return;
-  }
-  if (answer == "manuel") {
-    currentMode = manual;
-    return;
-  }
-  print("Données erronées");
-  currentMode = failure;
-  
-}
 
-void invokeAutomaticRedMode() {
-  print("Mode rouge");
-  digitalWrite(GREEN_LED, false);
-  digitalWrite(RED_LED, true);
-  levelCrossingGateClose(&gate);
-}
-
-void invokeAutomaticGreenMode() {
-  print("Mode vert");
-  digitalWrite(RED_LED, false);
-  digitalWrite(GREEN_LED, true);
-  levelCrossingGateOpen(&gate);
-  // TODO refactory that because call stack
-  String answer = input("rouge ou manuel ?");
-  if (answer == "rouge") {
-    invokeAutomaticRedMode();
-    return;
-  }
-  if (answer == "manuel") {
-    currentMode = manual;
-    return;
-  }
-  print("Données erronées");
-  currentMode = failure;
-}
-
-void invokeManualMode() {
-  print("invokeManualMode");
-}
 
 void blinkTwoLed() {
   unsigned long currentMillis = millis();
@@ -165,21 +206,6 @@ void blinkTwoLed() {
   }
 }
 
-void levelCrossingGateOpen(LevelCrossingGate *gate) {
-  if (gate->position == gate->minimum) {
-    return;
-  }
-  gate->position = gate->minimum;
-  gate->servo.write(gate->position);
-}
-
-void levelCrossingGateClose(LevelCrossingGate *gate) {
-  if (gate->position == gate->minimum) {
-    return;
-  }
-  gate->position = gate->middle;
-  gate->servo.write(gate->position);
-}
 
 String input(String toPrint="") {
   print(toPrint);
